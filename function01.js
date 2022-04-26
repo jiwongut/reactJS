bd94870a643aae4c3f887f71e5b0ca1348500da6
jiwon <79279500+jiwongut@users.noreply.github.com>
2022-04-26 19:49:45 +0900

    0426 js

---
+++ function01.js
@@ -0,0 +1,17 @@
+function getArea(width,height){ //매개변수 width,height
+    let area = width * height;
+    console.log(area);
+    return area;
+} //함수 선언식
+
+getArea(10,20); //호출시
+
+let hello = function(){
+    return "안녕하세요";
+}; //함수 표현식
+
+const hellotext = hello();
+console.log(hellotext);
+
+let helloA = () => "안녕하세요";
+console.log(helloA());
